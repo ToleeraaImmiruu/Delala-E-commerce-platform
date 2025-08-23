06f937b3e6f9b7d10427aff76174c3ccc3aafec8
Tolera Imiru <toleeraa2024@gmail.com>
2025-08-23 14:10:36 +0300

    Update AdminUsers component and use .env API URL

--- Backend/index.js
+++ Backend/index.js
@@ -45,7 +45,7 @@ app.use("/api", Cart);
 // app.use("/api", Pending);
 app.use("/api", adminModerationRoutes);
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT  || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
